75948699a562cbb0cea7487a8fe2ec41c0eedff3
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-05-03 15:06:15 +0000

    Finished webpage

--- results.php
+++ results.php
@@ -2,30 +2,37 @@
 
 	// get the user input
   $input = floatval($_POST['age']);
-  
+
+  // declare constant
+
+  define(FIRST_THRESHOLD, 18);
+  define(SECOND_THRESHOLD, 13);
+  define(THIRD_THRESHOLD, 12);
+
   // determine the message based on the age. 
    
-	if ($input > 18) {
+	if ($input >= FIRST_THRESHOLD) {
     
     echo 'You can see an R rated movie alone.';
     
 	}
 
-  else if ($input > 13){
+  else if ($input >= SECOND_THRESHOLD){
     
     echo 'You can see a PG-13 rated movie alone.';
     
   }
 
-  else if ($input > 6){
+  else if ($input >= THIRD_THRESHOLD){
     
     echo 'You can see a G rated movie alone.';
     
   }
 
-  else ($input < 5) {
+
+  else {
     
-    echo 'You should not be seeing a movie alone.';
+    echo 'You should not be seeing a movie alone. You need parental supervision.';
     
   } 
 ?>
